6813e3cc4a77e98f4bba555a21e9157e2bd0304b
SydBrain <103507827+SydBrain@users.noreply.github.com>
2023-10-23 19:34:46 +0200

    Completed basic game implementation with UI

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ document.addEventListener('DOMContentLoaded', () => {
   let playerSelection = "";
   let computerSelection = "";
 
+  const playerWins = "Player Wins!";
+  const computerWins = "Computer Wins!";
+  const itsADraw = "It's a Draw!";
+
   const selectionButtons = document.querySelectorAll('.selection-button');
+
+
+  const selectionsContainer = document.querySelector('.selections-container');
+  const startGameButton = document.querySelector('.start-button');
   const displayedPlayerScore = document.querySelector('.player-score');
   const displayedComputerScore = document.querySelector('.computer-score');
-
-  selectionButtons.forEach(button => {
-    button.addEventListener('click', () => game(button.value));
-  });
+  const displayedResult = document.querySelector('.result');
+  const roundOutcome = document.querySelector('.round-outcome')
 
   const choices = ["rock", "paper", "scissors"];
 
@@ -21,44 +27,68 @@ document.addEventListener('DOMContentLoaded', () => {
     scissors: { win: "paper", lose: "rock" },
   };
 
-  function game(selectedOption) {
+  startGameButton.addEventListener('click', () => {
+    startGameButton.classList.add('hidden');
+    selectionsContainer.classList.remove('hidden');
+    startGame();
+  });
+
+  function startGame() {
+
     playerScore = 0;
     computerScore = 0;
-
-    playerSelection = selectedOption;
-    computerSelection = getComputerChoice();
-
-    playRound(playerSelection, computerSelection);
 
     displayedPlayerScore.innerText = playerScore;
     displayedComputerScore.innerText = computerScore;
 
-    if (playerScore > computerScore) {
-      console.log(`Player wins!`);
-    } else if (playerScore < computerScore) {
-      console.log(`Computer wins!`);
-    } else {
-      console.log(`It's a draw`);
+    selectionButtons.forEach(button => {
+      button.addEventListener('click', (e) => {
+        playerSelection = e.target.parentNode.value;
+        computerSelection = getComputerChoice();
+
+        playRound(playerSelection, computerSelection);
+
+        displayedPlayerScore.innerText = playerScore;
+        displayedComputerScore.innerText = computerScore;
+        checkWinner();
+      });
+    });
+  }
+
+  function checkWinner() {
+    if (playerScore === 5 || computerScore === 5) {
+      if (playerScore > computerScore) {
+        displayedResult.innerText = playerWins;
+      } else if (playerScore < computerScore) {
+        displayedResult.innerText = computerWins;
+      } else {
+        displayedResult.innerText = itsADraw;
+      }
     }
   }
 
   function playRound(playerSelection, computerSelection) {
+    roundOutcome.innerText = "";
+    playerSelection.charAt(0).toUpperCase();
+    computerSelection.charAt(0).toUpperCase();
+
     if (playerSelection === computerSelection) {
-      console.log(`It's a draw!`);
+      roundOutcome.innerText = `It's a draw!`;
       return;
     }
 
     if (outcomes[playerSelection].win === computerSelection) {
-      console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+      roundOutcome.innerText = `You won! ${playerSelection} beats ${computerSelection}`;
       playerScore += 1;
       return;
     }
 
     if (outcomes[playerSelection].lose === computerSelection) {
-      console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+      roundOutcome.innerText = `You lose! ${computerSelection} beats ${playerSelection}`;
       computerScore += 1;
       return;
     }
+
   }
 
   function getComputerChoice() {
