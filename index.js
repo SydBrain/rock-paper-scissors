7fdff46cbdc4a6062773e0845ca5ae2e38209db6
SydBrain <103507827+SydBrain@users.noreply.github.com>
2023-10-23 13:22:23 +0200

    Implemented button functionality

--- index.js
+++ index.js
@@ -1,76 +1,71 @@
-let playerScore;
-let computerScore;
+document.addEventListener('DOMContentLoaded', () => {
+  let playerScore;
+  let computerScore;
 
-let playerSelection;
-let computerSelection;
+  let playerSelection = "";
+  let computerSelection = "";
 
-const choices = ["rock", "paper", "scissors"];
+  const selectionButtons = document.querySelectorAll('.selection-button');
 
-const outcomes = {
-  rock: { win: "scissors", lose: "paper" },
-  paper: { win: "rock", lose: "scissors" },
-  scissors: { win: "paper", lose: "rock" },
-};
+  selectionButtons.forEach(button => {
+    button.addEventListener('click', () => game(button.value));
+  });
 
-function game() {
-  playerScore = 0;
-  computerScore = 0;
+  const choices = ["rock", "paper", "scissors"];
 
-  for (let i = 0; i < 5; i++) {
-    playerSelection = "";
-    computerSelection = "";
+  const outcomes = {
+    rock: { win: "scissors", lose: "paper" },
+    paper: { win: "rock", lose: "scissors" },
+    scissors: { win: "paper", lose: "rock" },
+  };
 
-    /* while (!choices.includes(playerSelection)) {
-      playerSelection = prompt(`Rock, Paper or Scissors?`).toLowerCase().trim();
+  function game(selectedOption) {
+    playerScore = 0;
+    computerScore = 0;
 
-      if (!choices.includes(playerSelection)) {
-        alert(
-          `Invalid input, you have to choose between Rock, Paper or Scissors!`
-        );
-      }
-    } */
-
+    playerSelection = selectedOption;
     computerSelection = getComputerChoice();
 
     playRound(playerSelection, computerSelection);
 
     console.log(`The player score is: ${playerScore}`);
     console.log(`The computer score is: ${computerScore}`);
+
+
+    console.log(`Final player score: ${playerScore}`);
+    console.log(`Final computer score: ${computerScore}`);
+
+    if (playerScore > computerScore) {
+      console.log(`Player wins!`);
+    } else if (playerScore < computerScore) {
+      console.log(`Computer wins!`);
+    } else {
+      console.log(`It's a draw`);
+    }
   }
 
-  console.log(`Final player score: ${playerScore}`);
-  console.log(`Final computer score: ${computerScore}`);
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      console.log(`It's a draw!`);
+      return;
+    }
 
-  if (playerScore > computerScore) {
-    console.log(`Player wins!`);
-  } else if (playerScore < computerScore) {
-    console.log(`Computer wins!`);
-  } else {
-    console.log(`It's a draw`);
-  }
-}
+    if (outcomes[playerSelection].win === computerSelection) {
+      console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+      playerScore += 1;
+      return;
+    }
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    console.log(`It's a draw!`);
-    return;
-  }
-
-  if (outcomes[playerSelection].win === computerSelection) {
-    console.log(`You won! ${playerSelection} beats ${computerSelection}`);
-    playerScore += 1;
-    return;
+    if (outcomes[playerSelection].lose === computerSelection) {
+      console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+      computerScore += 1;
+      return;
+    }
   }
 
-  if (outcomes[playerSelection].lose === computerSelection) {
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-    computerScore += 1;
-    return;
+  function getComputerChoice() {
+    return choices[Math.floor(Math.random() * 3)];
   }
-}
 
-function getComputerChoice() {
-  return choices[Math.floor(Math.random() * 3)];
-}
+})
 
-game();
